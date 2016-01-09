2b9d9a249a898ffb78fd933d97e7888ad6cb38a2
Sunny <songkunpeng@jovision.com>
2016-01-09 16:17:29 +0800

    修改

--- XWJ/XWJFindPubViewController.m
+++ XWJ/XWJFindPubViewController.m
@@ -186,7 +186,7 @@
     UIImagePickerController * pick = [[UIImagePickerController alloc]init];
     pick.sourceType=type;
     pick.delegate=self;
-    pick.allowsEditing=self;
+    pick.allowsEditing=NO;
     [self presentViewController:pick animated:NO completion:nil];
     
 }

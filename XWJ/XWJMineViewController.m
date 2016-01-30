ea19c1a55de6a3eaf6c3cb798af202762d2ecbe7
聚城科技 <13176888155@163.com>
2016-01-30 14:07:04 +0800

    a

--- XWJ/XWJMineViewController.m
+++ XWJ/XWJMineViewController.m
@@ -45,7 +45,7 @@ NSArray *myImgs;
 //    
 //}
 -(void)viewDidLoad{
-    
+   
     NSDictionary *dicuser = [[NSDictionary alloc]init];
     [self downLoadData];
 //    self.tableData = [NSArray arrayWithObjects:@"关于信我家",@"修改密码",@"版本检查",@"修改建议" ,@"退出登录" ,nil];
@@ -388,17 +388,20 @@ NSArray *myImgs;
         UIAlertView * alertview = [[UIAlertView alloc] initWithTitle:nil message:@"确定要退出登陆？" delegate:self cancelButtonTitle:@"确定" otherButtonTitles:@"取消", nil];
         alertview.delegate = self;
         [alertview show];
-//        [[NSUserDefaults standardUserDefaults] removeObjectForKey:@"password"];
-//        [[NSUserDefaults standardUserDefaults] removeObjectForKey:@"username"];
-//        UIStoryboard *loginStoryboard = [UIStoryboard storyboardWithName:@"XWJLoginStoryboard" bundle:nil];
-//        self.view.window.rootViewController = [loginStoryboard instantiateInitialViewController];
     }
     
 }
 - (void)alertView:(UIAlertView *)alertView clickedButtonAtIndex:(NSInteger)buttonIndex{
     if (buttonIndex == 0) {
-        [[NSUserDefaults standardUserDefaults] removeObjectForKey:@"password"];
-        [[NSUserDefaults standardUserDefaults] removeObjectForKey:@"username"];
+        
+//清空所有的缓存到本地的信息
+        
+//        [[NSUserDefaults standardUserDefaults] removeObjectForKey:@"password"];
+//        [[NSUserDefaults standardUserDefaults] removeObjectForKey:@"username"];
+//        NSDictionary *defaultsDictionary = [[NSUserDefaults standardUserDefaults]dictionaryRepresentation];
+//        for (NSString *key in [defaultsDictionary allKeys]) {    [[NSUserDefaults standardUserDefaults] removeObjectForKey:key];
+//        }
+//        [[NSUserDefaults standardUserDefaults] synchronize];
         UIStoryboard *loginStoryboard = [UIStoryboard storyboardWithName:@"XWJLoginStoryboard" bundle:nil];
         self.view.window.rootViewController = [loginStoryboard instantiateInitialViewController];
     }

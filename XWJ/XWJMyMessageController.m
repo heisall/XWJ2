c905414e53eb03f074230717ca97b3ea3a81c828
CaoZhaohua <511123524@qq.com>
2016-01-31 09:29:09 +0800

    a

--- XWJ/XWJMyMessageController.m
+++ XWJ/XWJMyMessageController.m
@@ -140,50 +140,14 @@ static NSString *kcellIdentifier = @"cell";
             
             UIStoryboard *find = [UIStoryboard storyboardWithName:@"FindStoryboard" bundle:nil];
             XWJFindDetailViewController * con = [find instantiateViewControllerWithIdentifier:@"findDetail"];
-            //      con.finddetail = self.finddetailArr;
-            //       con.dic = [NSMutableDictionary dictionaryWithDictionary:(NSDictionary*) [self.finddetailArr objectAtIndex:indexPath.section*COLLECTION_NUMITEMS +indexPath.row]];
-            con.dic = self.finddetailArr[indexPath.row];
+
+           NSString * fromeId = [self.finddetailArr[indexPath.row]  objectForKey:@"fromid"];
+            NSMutableDictionary *dict = [[NSMutableDictionary alloc] init];
+           dict[@"id"] = fromeId;
+           con.dic  = dict;
             NSLog(@"****%@",con.dic);
             [self.navigationController showViewController:con sender:nil];
-            
-  
-
-   // [self downLoadFindData];
-//    NSString *messageUrl = @"http://www.hisenseplus.com:8100/appPhone/rest/find/findDetail";
-//    AFHTTPRequestOperationManager *manager = [AFHTTPRequestOperationManager manager];
-//    manager.responseSerializer.acceptableContentTypes = [manager.responseSerializer.acceptableContentTypes setByAddingObject:@"text/plain"];
-//    
-//    NSMutableDictionary *dict = [NSMutableDictionary dictionary];
-//    
-//    XWJAccount *account = [XWJAccount instance];
-//    [dict setValue:@"87"  forKey:@"id"];
-//    NSLog(@"%@",_idArray[indexPath.row]);
-//    [dict setValue:account.uid forKey:@"userid"];
-//    [manager POST:messageUrl parameters:dict success:^(AFHTTPRequestOperation *operation, id responseObject) {
-//        NSLog(@"dic***** %@",responseObject);
-//        
-//        if(responseObject){
-//            NSDictionary *dict = (NSDictionary *)responseObject;
-//            NSDictionary *dicc =[[NSDictionary alloc]init];
-//            dicc = [dict objectForKey:@"data"];
-//            self.dic = [dicc objectForKey:@"find"];
-//            NSLog(@"dic*****%@",self.dic);
-//            
-//            UIStoryboard *find = [UIStoryboard storyboardWithName:@"FindStoryboard" bundle:nil];
-//            XWJFindDetailViewController * con = [find instantiateViewControllerWithIdentifier:@"findDetail"];
-//            //      con.finddetail = self.finddetailArr;
-//            //       con.dic = [NSMutableDictionary dictionaryWithDictionary:(NSDictionary*) [self.finddetailArr objectAtIndex:indexPath.section*COLLECTION_NUMITEMS +indexPath.row]];
-//            con.dic = _dic;
-//            NSLog(@"****%@",con.dic);
-//            [self.navigationController showViewController:con sender:nil];
-//            
-//        }
-//        
-//    } failure:^(AFHTTPRequestOperation *operation, NSError *error) {
-//        NSLog(@"%s fail %@",__FUNCTION__,error);
-//        
-//    }];
-
+    
 }
 
 -(void)tableView:(UITableView *)tableView didDeselectRowAtIndexPath:(NSIndexPath *)indexPath{
